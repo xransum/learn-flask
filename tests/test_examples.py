a9869f63067f8bcea8162f679d6365c753109692
xransum <xransum@users.noreply.github.com>
2023-11-20 17:04:34 -0500

    Added linting and pytest

---
+++ tests/test_examples.py
@@ -0,0 +1,10 @@
+"""Empty test suite."""
+import unittest
+
+
+class TestNothing(unittest.TestCase):
+    """Test class for nothing."""
+
+    def test_nothing(self: unittest.TestCase) -> None:
+         """Nothing to test."""
+         self.assertTrue(True)
